8d3fc9a9fe6ea8c7d238632601cfe95394ed8373
Chris van Marle <qistoph@gmail.com>
2011-09-18 18:20:29 +0200

    Echos in mklinks aren't necessary anymore

--- mklinks.sh
+++ mklinks.sh
@@ -9,14 +9,7 @@ set -o errexit
 # Print each command to stdout before executing it
 set -o verbose
 
-echo linking to .bashrc
 ln -bs .settings/.bashrc .bashrc
-
-echo linking to .screenrc
 ln -bs .settings/.screenrc .screenrc
-
-echo linking to .vimrc
 ln -bs .settings/.vimrc .vimrc
-
-echo linking to skel.sh
 ln -bs .settings/skel.sh skel.sh
