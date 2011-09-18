85a4752af3ab1989282b7a77d2ac27a3fdc327c7
Chris van Marle <qistoph@gmail.com>
2011-09-18 17:40:08 +0200

    Added shell script skeleton file

--- mklinks.sh
+++ mklinks.sh
@@ -9,3 +9,5 @@ ln -bs .settings/.screenrc .screenrc
 echo linking to .vimrc
 ln -bs .settings/.vimrc .vimrc
 
+echo linking to skel.sh
+ln -bs .settings/skel.sh skel.sh
---
+++ skel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Exit if undeclared variables are used
+set -o nounset
+
+# Exit if any command exits with error
+set -o errexit
+
+# Print each command to stdout before executing it
+# set -o verbose
+
+
