8ddf643638371711ef6a618da202de7242d18f6a
Chris van Marle <qistoph@gmail.com>
2011-09-18 17:13:49 +0200

    Updated README added mklinks script

---
+++ mklinks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo linking to .bashrc
+ln -bs .settings/.bashrc .bashrc
+
+echo linking to .screenrc
+ln -bs .settings/.screenrc .screenrc
+
+echo linking to .vimrc
+ln -bs .settings/.vimrc .vimrc
+
