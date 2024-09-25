e038486df1f8b38dbcad53dea51f0094101f023e
Chris van Marle <qistoph@gmail.com>
2024-09-25 12:36:04 +0200

    Docs instead of comments

--- skel.py
+++ skel.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+
 def interact(_loc):
+    ''' Start an interactive debugger
+        Attach debugger with `interact(locals())` to access variables in scope
+    '''
+
     import code
     import readline
     import rlcompleter
